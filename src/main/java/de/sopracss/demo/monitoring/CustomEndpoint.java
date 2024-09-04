2b1989507a64cb9890e610799f8805dc63b6954c
Henner Storch <hstorch@it-economics.de>
2024-09-04 15:44:51 +0200

    custom actuator endpoint

---
+++ src/main/java/de/sopracss/demo/monitoring/CustomEndpoint.java
@@ -0,0 +1,29 @@
+package de.sopracss.demo.monitoring;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.stereotype.Component;
+
+@Component
+@Endpoint(id = "greet", enableByDefault = false)
+public class CustomEndpoint {
+
+
+
+    @ReadOperation //HTTP GET
+    public String customRead() {
+        return "Hello User";
+    }
+
+    @ReadOperation //HTTP GET
+    public String customReadPath(@Selector() String name) {
+        return "Hello " + name;
+    }
+
+    @WriteOperation //HTTP POST
+    public String customWrite(String user) {
+        return "Hello " + user;
+    }
+}
