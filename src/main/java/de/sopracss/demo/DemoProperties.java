56464beaf6528905a00cc554069be74a0c790993
Henner Storch <henner.storch@soprasteria.com>
2025-09-26 08:40:22 +0200

    upgrade Spring version upgrade Java Version upgrade to Gradle 9

--- src/main/java/de/sopracss/demo/DemoProperties.java
+++ src/main/java/de/sopracss/demo/DemoProperties.java
@@ -2,9 +2,7 @@ package de.sopracss.demo;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "demo")
 @Data
 public class DemoProperties {
