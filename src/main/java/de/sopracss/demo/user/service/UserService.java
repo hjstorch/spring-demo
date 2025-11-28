3b21fc10ebe9d6e5c15bf8bef603d2142a1931e0
Henner Storch <henner.storch@soprasteria.com>
2025-11-28 16:16:24 +0100

    migrate to Spring version 4/7 update to Java 25

--- src/main/java/de/sopracss/demo/user/service/UserService.java
+++ src/main/java/de/sopracss/demo/user/service/UserService.java
@@ -1,8 +1,6 @@
 package de.sopracss.demo.user.service;
 
 import de.sopracss.demo.user.model.User;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.*;
